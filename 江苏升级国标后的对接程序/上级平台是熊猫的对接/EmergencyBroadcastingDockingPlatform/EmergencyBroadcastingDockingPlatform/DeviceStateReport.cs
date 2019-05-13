e71457e6b2581b6f2cb7118aaca50556deec6e3e
CavanOrBxj <40481651+CavanOrBxj@users.noreply.github.com>
2019-05-13 08:35:08 +0800

    江苏项目国标升级后的平台对接_熊猫

---
+++ 江苏升级国标后的对接程序/上级平台是熊猫的对接/EmergencyBroadcastingDockingPlatform/EmergencyBroadcastingDockingPlatform/DeviceStateReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmergencyBroadcastingDockingPlatform
+{
+    [Serializable]
+    public class DeviceStateReport
+    {
+        public Device Device;
+    }
+}
